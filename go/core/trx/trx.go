27d675f4b1e2471ce223bd233c603809d19a5ce9
newset <luojieyy@gmail.com>
2023-07-11 21:23:50 +0800

    Update trx.go

--- go/core/trx/trx.go
+++ go/core/trx/trx.go
@@ -3,9 +3,11 @@ package trx
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"blockbase/core"
+	"fmt"
+	"go-demo/core"
 	"strings"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	trxProtoCore "github.com/fbsobreira/gotron-sdk/pkg/proto/core"
 	"github.com/golang/protobuf/proto"
@@ -15,6 +17,11 @@ const addressPrefix = 0x41
 
 const symbol = "TRX"
 
+const TRX_MESSAGE_HEADER = "\x19TRON Signed Message:\n32"
+
+// it should be: '\x15TRON Signed Message:\n32';
+const ETH_MESSAGE_HEADER = "\x19Ethereum Signed Message:\n32"
+
 // trx key derivation service
 type trx struct{ core.CoinInfo }
 
@@ -22,17 +29,17 @@ type trx struct{ core.CoinInfo }
 func SignHash(hash string, privateKeyHex string) (signature string) {
 	txRawBytes, err := hex.DecodeString(hash)
 	if err != nil {
-		return "hex error" + err.Error()
+		return "hex error " + err.Error()
 	}
 	tx := new(trxProtoCore.Transaction)
 	err = proto.Unmarshal(txRawBytes, tx)
 	if err != nil {
-		return "transaction error"
+		return "transaction error" + err.Error()
 	}
 
 	rawData, err := proto.Marshal(tx.GetRawData())
 	if err != nil {
-		return "raw data error"
+		return "raw data error" + err.Error()
 	}
 
 	privateKeyHex = strings.TrimPrefix(privateKeyHex, "0x")
@@ -43,6 +50,19 @@ func SignHash(hash string, privateKeyHex string) (signature string) {
 	tx.Signature = append(tx.Signature, sign)
 	txSigBytes, _ := proto.Marshal(tx)
 	return hex.EncodeToString(txSigBytes)
+}
+
+func SignString(message string, key string) (signature string) {
+
+	fullMessage := fmt.Sprintf("\x19TRON Signed Message:\n%d%s", 32, message)
+	hashData := crypto.Keccak256Hash([]byte(fullMessage))
+
+	privateKeyECDSA, _ := crypto.HexToECDSA(key)
+	sign, _ := crypto.Sign(hashData.Bytes(), privateKeyECDSA)
+
+	// https://stackoverflow.com/a/69771013
+	sign[64] += 27
+	return hexutil.Encode(sign)
 }
 
 // 签名交易
