f38a7847653fd5486bba71efb11735811b4902f7
newset <luojieyy@gmail.com>
2023-07-20 20:12:14 +0800

    Update lib.go

--- go/lib.go
+++ go/lib.go
@@ -1,18 +1,22 @@
 package main
 
-// #include <stdio.h>
-// #include <errno.h>
 import "C"
-import (
-	"blockbase/core/trx"
-)
 
-//export SignTron
-func SignTron(message *C.char, privateKey *C.char) *C.char {
-	sig := trx.SignString(C.GoString(message), C.GoString(privateKey))
-	return C.CString(sig)
-}
+import "blockbase/core/trx"
 
 func main() {
 
 }
+
+//export signTron
+func signTron(txId *C.char, privateKey *C.char) *C.char {
+	var sig = trx.SignString(C.GoString(txId), C.GoString(privateKey))
+	return C.CString(sig)
+}
+
+//export version
+func version() {
+	print("version: 1.0.0")
+}
+
+// https://cloud.tencent.com/developer/article/1786332
